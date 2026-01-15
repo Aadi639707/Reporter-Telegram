eee06f34cf0cfac24597a30aa2646090e83fc03a
Aadi639707 <aadi639707@gmail.com>
2026-01-15 11:10:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import time,os,platform
-try:from prettytable import PrettyTable
-except:os.system("pip install prettytable")
+import time, os, platform, subprocess, sys
+
+# Library install karne ka function
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Prettytable check aur install
+try:
+    from prettytable import PrettyTable
+except ImportError:
+    install('prettytable')
+    from prettytable import PrettyTable
+
+# Colorama check aur install
+try:
+    from colorama import init
+except ImportError:
+    install('colorama')
+    from colorama import init
+
+# Colors define karna
 rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[94m', '\033[01;35m'
-cn, k,g = '\033[00;36m', '\033[90m','\033[38;5;130m'
+cn, k, g = '\033[00;36m', '\033[90m', '\033[38;5;130m'
+
 def re(text):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(0.001)  
+
 if 'Windows' in platform.uname():
-    from colorama import init
     init()
-else:
-    pass
-banner = f"""
-                                                                 
-{k}                                                                
-                              -     -                            
+
+banner = f"""                                                                
+{k}                              -     -                            
                             .+       +.                          
                            :#         #:                         
                           =%           %-                        
            {lrd}REPORTER{k}     -*%.   {g}SpiDer{k}   .%+    {be}TELEGRAM      {k}       
                         #@:             -@#                      
-                     :  #@:             :@*  :                   
-                    -=  *@:             -@*  =-                  
+                     :  #@:             :@* :                   
+                    -=  *@:             -@* =-                  
                    -%   *@-             =@+   %-                 
                   -@=  .*@+             +@+.  =@-                
                  =@%   .+@%-    :.:    -@@+.   #@:               
@@ -31,9 +47,9 @@ banner = f"""
                     -##%%%%%#*@@@@@@@*#%%%%%##-                  
                   .*#######%@@@@@@@@@@@%#######*.                
                .=#@%*+=--#@@@@@@@@@@@@@@@#--=+*%@#=.             
-            .=#@%+:     *@@@@@+.   .+@@@@@*     :+%@#=.          
+            .=#@%+:     *@@@@@+.   .+@@@@@* :+%@#=.          
           :*@@=.    .=#@@@@@@@       @@@@@@@#=.    .=@@*.        
-            =@+    .%@@*%@@@@@*     *@@@@@%*@@%.    +@=          
+            =@+    .%@@*%@@@@@* *@@@@@%*@@%.    +@=          
              :@=    +@# :@@@@@#     #@@@@%. #@+    =@:           
               .#-   :@@  .%@@#       #@@#.  @@:   -#.            
                 +:   %@:   =%         %=   :@%   -+              
@@ -49,18 +65,25 @@ banner = f"""
 """
 
 re(banner)
-re("Warning ! This is a test reporter, any offense is the responsibility of the user !")
-print (f"{lrd}")
-t = PrettyTable([f'{cn}Number{lrd}',f'{cn}info{lrd}'])
-t.add_row([f'{lgn}1{lrd}',f'{gn}Reporter Channel{lrd}'])
-t.add_row([f'{lgn}2{lrd}',f'{gn}Reporter Account{lrd}'])
-t.add_row([f'{lgn}3{lrd}',f'{gn}Reporter Group [Updating]{lrd}'])
-print (t)
+re("Warning ! This is a test reporter, any offense is the responsibility of the user !\n")
 
-number = input(f"{gn}Enter Number : {cn}")
-if number == "1":
-    os.system("python report/reporter.py")
-if number == "2":
-	os.system("python report/report.py")
-elif number == "3":
-    print ("This section is being updated and will be added soon \n\nChannel : @esfelurm")
+print(f"{lrd}")
+t = PrettyTable([f'{cn}Number{lrd}', f'{cn}info{lrd}'])
+t.add_row([f'{lgn}1{lrd}', f'{gn}Reporter Channel{lrd}'])
+t.add_row([f'{lgn}2{lrd}', f'{gn}Reporter Account{lrd}'])
+t.add_row([f'{lgn}3{lrd}', f'{gn}Reporter Group [Updating]{lrd}'])
+print(t)
+
+# Render par input kaam nahi karega agar aap "Background Worker" nahi chala rahe
+# Lekin script test karne ke liye ye sahi hai
+try:
+    number = input(f"{gn}Enter Number : {cn}")
+    if number == "1":
+        os.system("python report/reporter.py")
+    elif number == "2":
+        os.system("python report/report.py")
+    elif number == "3":
+        print("This section is being updated and will be added soon \n\nChannel : @esfelurm")
+except EOFError:
+    print(f"\n{lrd}Error: Render web service par user input support nahi hota.")
+	
